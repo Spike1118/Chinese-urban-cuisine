dae13f90350d97f2ba5dfc7446cd9eee28340b86
Spike1118 <3270556449@qq.com>
2026-03-19 09:39:30 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vuejs/vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  // 👇 就加这一行！必须加！
-  base: '/Chinese-urban-cuisine/'
-})
+	plugins: [vue()],
+	server: {
+		port: 3000,
+		open: true,
+	},
+	// 👇 就加这一行！必须加！
+	base: "/Chinese-urban-cuisine/",
+});
